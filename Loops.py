0d34bdb5bd9e561bdaff355a131178c012f34dcc
takashk <kyoko.takashima.5@gmail.com>
2017-02-01 21:17:11 -0800

    Add files via upload

---
+++ Loops.py
@@ -0,0 +1,41 @@
+# for number in range(1, 21):
+#     if number == 13:
+#         print "hello"
+#     else:
+#         print number
+
+#fruits = ["apples", "oranges", "bananas"]
+
+# for item in fruits:
+#     print item
+
+# ex = range(len(fruits))
+# print ex
+# for i in ex:
+#     print i
+#     print fruits[i]
+
+
+# def sum_num(num):
+#     print "RANGE", range(num)
+#     sum_of_num = 0
+#     for number in range(num):
+#         print "NUMBER: ",number
+        
+#         sum_of_num += number
+#         print "SUM OF NUM ",sum_of_num
+
+
+#     print "FINAL SUM", sum_of_num
+# sum_num(4)
+
+        # print add.sum_num[number]
+
+def sum_num2(num):
+    if sum_num2 > 0:
+        for number in range(num, 0):
+            print number
+            sum_num2 += number
+            print sum_num2
+
+sum_num2(-4)
