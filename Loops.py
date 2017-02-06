5bcd2b044314bc9104464c3b2630b02d51b86976
takashk <kyoko.takashima.5@gmail.com>
2017-02-05 18:25:46 -0800

    Add files via upload

--- Loops.py
+++ Loops.py
@@ -31,11 +31,93 @@
 
         # print add.sum_num[number]
 
-def sum_num2(num):
-    if sum_num2 > 0:
-        for number in range(num, 0):
-            print number
-            sum_num2 += number
-            print sum_num2
+#Not complete
+# def sum_num2(num):
+#     if sum_num2 > 0:
+#         for number in range(num, 0):
+#             print number
+#             sum_num2 += number
+#             print sum_num2
 
-sum_num2(-4)
+# sum_num2(-3)
+
+# Redoing the Exercise Part 2 For Loops
+# for my_list in range(1, 21):
+#     if my_list == 13:
+#         print "hello"
+#     else:
+#         print my_list,
+
+# Part 3 #3
+# my_list = ["apples", "oranges", "bananas"]
+# # print my_list
+
+# item = range(len(my_list))
+# print (item)
+
+# for element in item: 
+#     print my_list[element],
+
+# Part 3 #4
+# def sum_num(num):
+#     # print "RANGE %s" % range(num)
+#     sum_of_num = 0 
+#     for number in range(num):
+#         # print "NUMBER %s" % number
+#         sum_of_num += number
+#         print "SUM OF NUM ",sum_of_num
+#     return sum_of_num 
+
+# value = sum_num(4)
+# print "FINAL SUM ",value
+
+
+# def sum_num2(num):
+#     if num < 0:
+#         sum_of_num2 = 0
+#         for number in range(0, num, -1):
+#             print number
+#             sum_of_num2 += number
+#             print sum_of_num2
+#         return sum_of_num2
+#     else:
+#         return 0
+
+# value2 = sum_num2(-5)
+# print value2
+    
+#While loop Exercise 1
+# count = 1
+# while count < 21:
+#     if count == 13:
+#         print "hello"
+#     else:
+#         print count,
+#     count = count + 1
+
+# count = 0
+# while  count <= 100:
+#         print count,
+#         count = count + 10
+        
+my_list = ["apples", "oranges", "bananas"]
+
+index = 0
+while index<len(my_list):
+    print my_list[index]
+    index += 1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
