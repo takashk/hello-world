a114e57dccd92600942b2af2ab8d078d73a6fb8b
takashk <kyoko.takashima.5@gmail.com>
2017-02-06 21:21:21 -0800

    Add files via upload

---
+++ tuples.py
@@ -0,0 +1,38 @@
+# my_tuple = (1, 2, "kitten",  4, "five")
+# # print my_tuple[2]
+# # print my_tuple[3]
+# print my_tuple[4]
+# print my_tuple[-1]
+# print my_tuple[len(my_tuple)-1]
+# print len(my_tuple)
+# print my_tuple[4:]
+# print my_tuple + (6,)
+
+
+# my_tuple = (1, 2, "kitten",  4, "five", 6, 7)
+# print my_tuple[0]
+# print my_tuple[2:3]
+# print my_tuple[1:5]
+# print my_tuple[1:10]
+# print my_tuple[-1]
+# print my_tuple[-1:-4]
+# print my_tuple[-4:]
+
+# print my_tuple[-4:-1]
+
+# vocubulary_words = {
+#     "loops": "loops are a shortcut that allows you to perform the same set of tasks over again",
+#     "for loops": "when we know the number of times we will loop",
+#     "while loops": "when we don't know the number of times we will loop",
+#     "item": "a variable we use to keep track of where we are",
+# }
+
+prices = {"apples": 2, "bananas": 3, "oranges": 4, "pears": 5}
+    for key, value in prices.items():
+        if item1 < item2
+            print item2
+        else:
+            print 
+
+    # for item in prices(4):
+    #     price of item > next item
