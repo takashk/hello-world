0e07ec12c642a4f2705c82bc4a899d8171c999c8
takashk <kyoko.takashima.5@gmail.com>
2017-02-08 21:13:53 -0800

    Add files via upload

---
+++ shopping_list.py
@@ -0,0 +1,56 @@
+
+main_menu_dictionary = {
+    0: "Main Menu",
+    1: "Show all lists",
+    2: "Show a specific list",
+    3: "Add a new shopping list",
+    4: "Add an item to a shopping list",
+    5: "Remove an item from a shopping list",
+    6: "Remove a list by nickname",
+    7: "Exit",
+}
+
+shopping_list_dictionary = {
+    "target": "socks, soap, detergent, sponges",
+    "birite": "butter, cake, cookies, bread",
+    "berkeleybowl": "apples, oranges, bananas, milk",
+    "safeway": "oreos, brownies, soda",
+}
+
+#make it print prettier
+print main_menu_dictionary,
+# user_choice = int(raw_input("Select an option: "))
+
+# print main_menu_dictionary[user_choice]
+
+# def main_menu():
+#     print main_menu_dictionary
+#     # input 
+#     # call function
+#     # return to main menu 
+
+
+# def show_all_list():
+#     print shopping_list_dictionary, "Enter 0 to return to main menu."
+
+
+
+#     print shopping_list_dictionary.keys()
+#     user_list_choice = raw_input("Enter list nickname: ")
+#     shopping_list_dictionary[user_list_choice]
+
+
+# def add_new_list():
+#     print shopping_list_dictionary[raw_input "enter list nickname"] = []
+#     # ask user if you want to add item or return to main menu
+
+# def add_new_item():
+
+
+# def remove_item():
+#      print
+
+# def remove_list_by_nickname():
+#     print shopping_list_dictionary(raw_input"enter the nickname for the list you want to remove: ")
+
+# def exit():
